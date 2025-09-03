f616b2e9b2ea0be9eb98cd3f7dfd093d699b7031
JeffRasa <jeff.rasamison@univ-rouen.fr>
2025-09-03 17:11:25 +0200

    exercice 3

--- prog1.py
+++ prog1.py
@@ -1,1 +1,12 @@
-print("Hello, World!")
+import fonctions as f
+
+try:
+    a = int(input('a:'))
+    b = int(input('b:'))
+    res = f.puissance(a, b)
+    print(res)
+except TypeError as e:
+    print(f"Erreur : {e}")
+except ValueError:
+    print("Erreur : veuillez entrer des nombres entiers valides.")
+
