3aa0c2cc85027074eeb1ac2b3d31ba39e2f04256
JeffRasa <jeff.rasamison@univ-rouen.fr>
2025-09-05 13:23:11 +0200

    Verification du lancement d'une exeption

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,17 +1,25 @@
 def puissance(a, b):
 	if not isinstance(a, int):
-		raise TypeError("Only integers are allowed for 'a'")
+		raise TypeError("Seulement INT pour 'a'")
 	if not isinstance(b, int):
-		raise TypeError("Only integers are allowed for 'b'")
+		raise TypeError("Seulement INT pour 'b'")
 
-# Cas spécial : exposant nul
+	# Cas spécial : a = 0 et b = -1
+	if a == 0 and b == -1:
+		raise ValueError("Cas indéfini : 0 à la puissance -1")
+
+	# Cas spécial : a = 0 et b = 0
+	if a == 0 and b == 0:
+		raise ValueError("0 puissance 0 est une forme indéterminée")
+
+	# Cas spécial : exposant nul
 	if b == 0:
 		return 1
 
-# Cas d'exposant négatif
+	# Cas d'exposant négatif
 	if b < 0:
 		if a == 0:
-			raise ZeroDivisionError("0 cannot be raised to a negative power")
+			raise ZeroDivisionError("0 ne peut pas être élevé à une puissance négative")
 		result = 1
 		for _ in range(-b):
 			result *= a
--- TP1/test.py
+++ TP1/test.py
@@ -16,3 +16,7 @@ def test_3():
 # 0^x == 0 pour tout x > 0
 	for i in range(1,100):
 		assert f.puissance(0, i) == 0
+
+def test_exc_1():
+	with pytest.raises(Exception):
+		puissance(0,-1)
