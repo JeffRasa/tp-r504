c6b5dd4cbe3ea99bb0070b167d121f1cbc580752
JeffRasa <jeff.rasamison@univ-rouen.fr>
2025-09-22 10:55:36 +0200

    Update clientudp.java

--- TP9/clientudp.java
+++ TP9/clientudp.java
@@ -1,12 +1,27 @@
 import java.io.*;
 import java.net.*;
 
+public class ClientUDP {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            String s = "Hello World";
+            byte[] data = s.getBytes();
 
-InetAddress addr = InetAddress.getLocalHost():
-System.out.println( "adresse=" +addr.getHostName() );
+            InetAddress serveur = InetAddress.getByName("localhost");
+            DatagramPacket paquet = new DatagramPacket(data, data.length, serveur, 5000);
+            socket.send(paquet);
 
-DatagramPacket packet = new DatagramPacket ( data, data.length, addr, 1234 );
-DatagramSocket sock = new DatagramSocket ();
-sock.send(packet);
-sock.close();
+            // attendre la réponse du serveur (Q2.3)
+            byte[] buffer = new byte[1024];
+            DatagramPacket reponse = new DatagramPacket(buffer, buffer.length);
+            socket.receive(reponse);
+            String msg = new String(reponse.getData(), 0, reponse.getLength());
+            System.out.println("Réponse du serveur : " + msg);
 
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
