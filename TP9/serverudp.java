fedbb7781c0e16a2aba65f7eccf90efd4849744a
JeffRasa <jeff.rasamison@univ-rouen.fr>
2025-09-23 08:51:16 +0200

    Update serverudp.java

--- TP9/serverudp.java
+++ TP9/serverudp.java
@@ -1,28 +1,14 @@
-import java.io.*;
 import java.net.*;
 
-public class ServeurUDP {
-    public static void main(String[] args) {
-        try {
-            DatagramSocket socket = new DatagramSocket(5000); // port choisi
-            byte[] buffer = new byte[1024];
-            System.out.println("Serveur UDP en attente sur le port 5000...");
-
-            while (true) {
-                DatagramPacket paquet = new DatagramPacket(buffer, buffer.length);
-                socket.receive(paquet); // attente bloquante
-                String message = new String(paquet.getData(), 0, paquet.getLength());
-                System.out.println("Reçu : " + message);
-
-                // (Q2.3) renvoyer la chaîne au client
-                InetAddress adresseClient = paquet.getAddress();
-                int portClient = paquet.getPort();
-                DatagramPacket retour = new DatagramPacket(
-                        message.getBytes(), message.length(), adresseClient, portClient);
-                socket.send(retour);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+public class UdpReceiver {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket sock = new DatagramSocket(1234);
+        while (true) {
+            System.out.println("-Waiting data");
+            DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+            sock.receive(packet);
+            String str = new String(packet.getData());
+            System.out.println("str=" + str);
         }
     }
 }
